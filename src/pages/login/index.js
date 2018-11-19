096b7cb9909754744fa6ab02c56416b571824ca6
MarioMu <mfm@vip.qq.com>
2018-11-19 18:11:41 +0800

    Add Login Form

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,9 +1,48 @@
+import { Button, Checkbox, Form, Icon, Input } from 'antd';
 import styles from './index.css';
 
-export default function() {
-  return (
-    <div className={styles.normal}>
-      <h1>Page login</h1>
-    </div>
-  );
+const FormItem = Form.Item;
+
+class LoginForm extends React.Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  }
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <div className={styles.normal}>
+        <Form onSubmit={this.handleSubmit} className="login-form">
+          <FormItem>
+            {getFieldDecorator('userName', {
+              rules: [{ required: true, message: 'Please input your username!' }],
+            })(
+              <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+            )}
+          </FormItem>
+          <FormItem>
+            {getFieldDecorator('password', {
+              rules: [{ required: true, message: 'Please input your Password!' }],
+            })(
+              <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+            )}
+          </FormItem>
+          <FormItem>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              登录系统
+            </Button>
+          </FormItem>
+        </Form>
+      </div>
+    );
+  }
 }
+
+const WrappedLoginForm = Form.create()(LoginForm);
+
+export default WrappedLoginForm;
